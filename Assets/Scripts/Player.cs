c13ad542d9f2e2781a8c0294930efb1803eb98e3
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2023-10-10 23:59:07 +0100

    Refactoring movement code
    Changing movement code to move relative to where the player is facing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] private float movSpeed = 7f;
-    //[SerializeField] private GameInput gameInput;
+    [SerializeField] private float rotateSpeed = 12f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,21 +18,27 @@ public class Player : MonoBehaviour {
     {
         HandleMovement();
     }
-
     private void HandleMovement() {
-        Vector2 inputVector = Vector2.zero;
+        //creates a movement vector from player's input
+        Vector3 movDir = Vector3.zero;
         if (Input.GetKey("w")) {
-            inputVector.x = 1;
+            movDir += transform.forward;
         }if (Input.GetKey("a")) {
-            inputVector.y = 1;
+            movDir -= transform.right;
         }if(Input.GetKey("s")) {
-            inputVector.x = -1;
+            movDir -= transform.forward;
         }if(Input.GetKey("d")) {
-            inputVector.y = -1;
+            movDir += transform.right;
         }
-        inputVector = inputVector.normalized;
-        Vector3 movDir = new Vector3(inputVector.x,0,inputVector.y);
+        //normalises vector so player moves at the same speed diagonally
+        movDir = movDir.normalized;
+        //translates input vector to 3D space
+        //Vector3 movDir = new Vector3(inputVector.x,0f,inputVector.y);
 
-       transform.position += movDir*Time.deltaTime*movSpeed;
+        //updates player's pos  direction they're facing
+        transform.position += movDir * Time.deltaTime * movSpeed;
+        //transform.forward = Vector3.Slerp(transform.forward,movDir,Time.deltaTime*rotateSpeed);
+
+
     }
 }
