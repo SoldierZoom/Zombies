e8698535e12a19bc90485df3f49b09a41b07ae97
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2023-10-16 20:59:10 +0100

    Redoing movement and camera code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,44 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField] private CharacterController characterController;
     [SerializeField] private float movSpeed = 7f;
-    [SerializeField] private float rotateSpeed = 12f;
+    private float sprintMultiplier;
+
+    [SerializeField] private float gravityMag = -9.8f;
+    [SerializeField] private Transform groundCheck;
+    float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float jumpStrength=10f;
+
+    Vector3 v;
+    bool isGrounded;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         HandleMovement();
     }
     private void HandleMovement() {
-        //creates a movement vector from player's input
-        Vector3 movDir = Vector3.zero;
-        if (Input.GetKey("w")) {
-            movDir += transform.forward;
-        }if (Input.GetKey("a")) {
-            movDir -= transform.right;
-        }if(Input.GetKey("s")) {
-            movDir -= transform.forward;
-        }if(Input.GetKey("d")) {
-            movDir += transform.right;
+        //creates sphere at bottom of player to check if they have reached the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,groundLayer);
+        //sets velocity to -2 when player reaches the ground
+        if(isGrounded && v.y<0) {
+            v.y = -2f;
         }
-        //normalises vector so player moves at the same speed diagonally
-        movDir = movDir.normalized;
-        //translates input vector to 3D space
-        //Vector3 movDir = new Vector3(inputVector.x,0f,inputVector.y);
+        //reseting sprint multiplier
+        sprintMultiplier = 1f;
+        //creates movement vector relative to the player's position based on their input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movDir = transform.right * x + transform.forward * z;
+        //adds sprint multiplier if sprintkey pressed
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            sprintMultiplier = 3f;
+        }
+        //provides charcter controller with movement vector for player
+        characterController.Move(movDir*movSpeed*sprintMultiplier*Time.deltaTime);
 
-        //updates player's pos  direction they're facing
-        transform.position += movDir * Time.deltaTime * movSpeed;
-        //transform.forward = Vector3.Slerp(transform.forward,movDir,Time.deltaTime*rotateSpeed);
+        if(isGrounded && Input.GetButtonDown("Jump")) {
+            v.y = Mathf.Sqrt(jumpStrength * -2f * gravityMag);
+        }
 
 
+
+        //adds gravity to player
+        v.y += gravityMag * Time.deltaTime;
+        characterController.Move(v * Time.deltaTime);
     }
 }
