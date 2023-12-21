9d4275805843bc71c9c3c93e5bbd370e76cda9b2
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2023-12-21 02:11:42 +0000

    Updating list of vars for player animator to track

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -13,5 +13,6 @@ public class PlayerAnimator : MonoBehaviour {
         animator.SetBool("isSprinting",player.IsSprinting);
         animator.SetBool("isJumping",player.IsJumping);
         animator.SetBool("isAttacking",player.IsAttacking);
+        animator.SetBool("isMelee",player.IsMelee);
     }
 }
