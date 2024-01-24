46c6434f6073e4c758a7fadfdb2f91dd0e3f09ee
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2024-01-24 23:05:42 +0000

    Adding in ammo logic to scriptable object for ranged weapons

--- Assets/Scripts/RangedWeaponSO.cs
+++ Assets/Scripts/RangedWeaponSO.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class RangedWeaponSO:ScriptableObject {
     [SerializeField] private Transform prefab;
     [SerializeField] private string objectName;
-    private int ammoInGun;
+    private int ammo,ammoInGun;
     [SerializeField] private int maxAmmoInGun, maxShootingRange;
+    [SerializeField] private float shootDelay;
 
+    //get functions
     public Transform GetPrefab() {
         return prefab;
     }
@@ -20,6 +22,32 @@ public class RangedWeaponSO:ScriptableObject {
     }
     public int GetMaxShootingRange() {
         return maxShootingRange;
+    }
+    public float GetShootDelay() {
+        return shootDelay;
+    }
+    //ammo functions
+    public void Shoot() {
+        ammoInGun -= 1;
+        if(ammoInGun == 0) {
+            Reload();
+        }
+    }
+    public void Reload() {
+        //checking if gun is already fully loaded and player has ammo to reload
+        if (ammoInGun!=maxAmmoInGun&&ammo!=0) {
+            //subtracting the amount that needs to be loaded from ammo
+            ammo -= (maxAmmoInGun - ammoInGun);
+            //checking if gun can be fully loaded
+            if(ammo >= 0) {
+                ammoInGun = maxAmmoInGun;
+            }
+            //if not partially loading gun with what is remaining
+            else {
+                ammoInGun = maxAmmoInGun + ammo;
+                ammo = 0;
+            }
+        } 
     }
 
 }
