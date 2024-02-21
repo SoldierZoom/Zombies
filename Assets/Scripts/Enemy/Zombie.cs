6e55a4c116d2a7f8d927022482475b1bc4df09bb
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2024-02-21 11:59:19 +0000

    Created basic zombie script to follow player and play attacking animation when within range of player

---
+++ Assets/Scripts/Enemy/Zombie.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour {
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform player;
+    [SerializeField] private LayerMask playerLayer;
+    private float attackRange = 2;
+    private bool isWalking, isSprinting, isAttacking;
+    private int health;
+    void Start() {
+        health = 75;
+        this.gameObject.SetActive(true);
+    }
+    void Update() {
+        isAttacking = false;
+        HandleMovement();
+        if(Physics.CheckSphere(transform.position,attackRange,playerLayer)) {
+            AttackPlayer();
+        }
+    }
+
+    private void HandleMovement() {
+        isWalking = true;
+        agent.SetDestination(player.position);
+        
+    }
+    private void AttackPlayer() {
+        transform.LookAt(player.position);
+        isAttacking=true;
+    }
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("test");
+    }
+    void OnCollisionStay(Collision collision) {
+        Debug.Log(collision.gameObject.layer.ToString());
+        if(collision.gameObject.layer == playerLayer) {
+            Debug.Log("Hitting player");
+            //player.damage(1);
+        }
+    }
+
+    //anim get functions
+    public bool IsWalking { get { return isWalking; } }
+    public bool IsSprinting { get { return isSprinting; } }
+    public bool IsAttacking { get { return isAttacking; } }
+    //
+    public void Damage(int dmg) {
+        if((health-=dmg)>0) {
+            health-=dmg;
+        } else {
+            health = 0;
+            this.gameObject.SetActive(false);
+        }
+        Debug.Log("Zombie health now: " + health.ToString());
+    }
+}
