ff7fc0256893203238f8b1f896f2d2070f2fec4a
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2024-02-22 22:28:41 +0000

    misc

--- Assets/Scripts/Weapon/MeleeWeaponScriptObj.cs
+++ Assets/Scripts/Weapon/MeleeWeaponScriptObj.cs
@@ -19,9 +19,10 @@ public class MeleeWeaponScriptObj:ScriptableObject {
         return isOneHanded; 
     }
     public void Attack(Vector3 pos,Vector3 dirVector,LayerMask hittableLayerMask) {
-        if(Physics.CapsuleCast(pos,pos,2,dirVector,out RaycastHit hitInfo,2,hittableLayerMask)) {
+        if(Physics.CapsuleCast(pos,pos,0.5f,dirVector,out RaycastHit hitInfo,2,hittableLayerMask)) {
             if(hitInfo.transform.gameObject.layer == 8) {
                 hitInfo.transform.root.gameObject.GetComponent<Zombie>().Damage(damage);
+                Debug.Log("Enenemy Hit!");
             }
         }
     }
